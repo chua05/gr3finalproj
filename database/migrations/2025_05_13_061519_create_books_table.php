ebc70cbf99c78c3b0964fcf2f063f790ab62a01e
cruzetjhazel <cruzetjhazel@gmail.com>
2025-05-19 21:58:04 +0800

    Updated books_table in database migrations

--- database/migrations/2025_05_13_061519_create_books_table.php
+++ database/migrations/2025_05_13_061519_create_books_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->id('BookId');
+            $table->id();
             $table->string('Title');
             $table->string('Author');
             $table->string('Category');
