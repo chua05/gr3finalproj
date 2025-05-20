e8fe56b71aaa679a55998b77552c4428ae09a037
gonzales <cruzetjhazel@gmail.com>
2025-05-21 01:10:01 +0800

    update index.php public

--- public/index.php
+++ public/index.php
@@ -11,7 +11,9 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/laravel/vendor/autoload.php';
+$app = require_once __DIR__.'/laravel/bootstrap/app.php';
+
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
